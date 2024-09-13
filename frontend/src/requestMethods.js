204e04387ef78fd2427db93d1ce392b30698cef3
Abuzar <starkeqbal@gmail.com>
2024-09-13 17:20:03 +0530

    fixing 2

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const LocalBASE_URL = "http://localhost:8000/api/";
-const BASE_URL = "https://notespreader.onrender.com/";
+const BASE_URL = "https://notespreader.onrender.com";
 export const pf = "https://notespreader.onrender.com/images";
 
 export const publicRequest = axios.create({
